41e81eccf70353a93ef46e434522776d9875d328
iamfrs <farissalman.k289@gmail.com>
2024-05-02 12:12:26 +0530

    check 80 its defult

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (_req, res) => {
   res.send("hello man");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
