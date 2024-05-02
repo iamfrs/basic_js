502745493e4d4dfd660359c43a06f4dfb0d2831f
iamfrs <farissalman.k289@gmail.com>
2024-05-02 10:59:59 +0530

    check mongo

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const mongoose = require('mongoose');
 const authRoutes = require('./app/routers/loginRouter');
 require('dotenv').config();
 
-mongoose
-  .connect("mongodb://localhost:27017/test_dev")
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose
+//   .connect("mongodb://localhost:27017/test_dev")
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(express.json());
 const fileUpload = require("express-fileupload");
 app.use(fileUpload());
 
 app.use('/api', authRoutes);
+app.get("/", (_req, res) => {
+  res.send("hello man");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
